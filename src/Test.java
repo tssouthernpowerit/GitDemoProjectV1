765ccb5c7a343e762d93d2df3520a62c3a77ea7d
admin <admin@KIRAN>
2023-08-07 13:10:49 +0530

    changed. NEW.

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@
 public class Test {
 	public static void main(String args[]) {
 		
-		System.out.println("This is my first Git Repository");
+		System.out.println("This is my first Git Repositoryy");
 	}
 }
--- src/Test1.java
+++ src/Test1.java
@@ -2,8 +2,14 @@
 public class Test1 {
 	public static void main(String args[]) {
 		
-		System.out.println("This is my latest modifications Repository");
+		System.out.println("This is my latest modifications Repositoryy");
 		System.out.println("Git Sample Program");
 		System.out.println("Git Sample Bhavana");
+		
+		System.out.println("Git Sample Bhavana");
+		
+		int a = 10;
+
+
 	}
 }
